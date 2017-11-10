13116776f96134317f3c341787d856a56441a896
tobiaszirngibl <tobias.zirngibl3011@googlemail.com>
2017-11-10 17:32:57 +0100

    Added images, changed some texts

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -6,7 +6,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'yourmail@gmail.com'; //replace with your email
+	$email_to = 'tobiaszirngibl@gmx.de'; //replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
